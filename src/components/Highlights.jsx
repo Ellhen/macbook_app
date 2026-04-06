f6dbf74f6c63ac852dfe1139343b51e0d8d9e5f0
Ella <ella.tseluiko@gmail.com>
2026-04-05 19:01:03 -0500

    Implement Footer

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -59,7 +59,7 @@ const Highlights = () => {
 						<p>
 							Up to <span className="green-gradient"> 14 more hours </span>
 							battery life.{' '}
-							<span class="text-dark-100">(Up to 24 hours total.)</span>
+							<span className="text-dark-100">(Up to 24 hours total.)</span>
 						</p>
 					</div>
 				</div>
